93686c626612b4d3740122bca459beb33d24c36b
jun <ndh001@github.com>
2023-08-27 21:16:58 +0800

    last check

--- migrates.sql
+++ migrates.sql
@@ -1,7 +1,7 @@
-\copy traveldata.cities from '~/travelWeb_scrapper/csv/cities_n_links.csv' delimiter ',' header csv;
-\copy traveldata.foods from '~/travelWeb_scrapper/csv/food_all.csv' delimiter ',' header csv;
-\copy traveldata.shops from '~/travelWeb_scrapper/csv/shopping_all.csv' delimiter ',' header csv;
-\copy traveldata.sights from '~/travelWeb_scrapper/csv/sight_all.csv' delimiter ',' header csv;
-\copy traveldata.sights_texts from '~/travelWeb_scrapper/csv/sight_text_all.csv' delimiter ',' header csv;
-\copy traveldata.sights_imgs from '~/travelWeb_scrapper/csv/sight_imgs_all.csv' delimiter ',' header csv;
+\copy traveldata.cities from 'csv/cities_n_links.csv' delimiter ',' header csv;
+\copy traveldata.foods from 'csv/food_all.csv' delimiter ',' header csv;
+\copy traveldata.shops from 'csv/shopping_all.csv' delimiter ',' header csv;
+\copy traveldata.sights from 'csv/sight_all.csv' delimiter ',' header csv;
+\copy traveldata.sights_texts from 'csv/sight_text_all.csv' delimiter ',' header csv;
+\copy traveldata.sights_imgs from 'csv/sight_imgs_all.csv' delimiter ',' header csv;
 
